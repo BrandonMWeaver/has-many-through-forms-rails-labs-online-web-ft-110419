ff74bd022e97b0e4af4238840f7d1cfba1640aff
Brandon Weaver <a7xncob@gmail.com>
2020-01-29 00:30:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
+      unless category
       category = Category.find_or_create_by(category_attribute)
       self.categories << category
     end
